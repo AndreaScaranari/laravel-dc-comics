629d96e6ef2fd69f435f0879c8f679164f5b643f
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-21 03:16:24 +0100

    graphic_fixes_button_back

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -14,11 +14,14 @@
                 <h3 class="text-center">{{ $comic->type }}</h3>
             </div>
 
-            <div class="d-flex py-4 mx-auto justify-content-evenly ">
+            <div class="d-flex py-4 mx-auto justify-content-evenly" style="height:400px">
                 <img src="{{ $comic->thumb }}" alt="{{ $comic->series }}" class="img-fluid col-5">
-                <div class="col-5">
+                <div class="col-5 overflow-y-auto">
                     {{ $comic->description }}
                 </div>
+            </div>
+            <div class="d-flex justify-content-evenly">
+                <a href="{{ route('comics.index') }}" class="btn btn-info">Go Back!</a>
             </div>
         </div>
     </main>
