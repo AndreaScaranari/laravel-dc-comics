bbd033020a2c0b88eb12822e8faaa48fbde074f7
AndreaScaranari <andrea.scaranari.93@gmail.com>
2024-03-24 15:01:04 +0100

    routes_ordered

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,12 @@ Route::get('/', function () {
 });
 
 Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
+Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 Route::get('/comics/create', [ComicController::class, 'create'])->name('comics.create');
 Route::get('/comics/{comic}', [ComicController::class, 'show'])->name('comics.show');
-Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('teams.edit');
-Route::post('/comics', [ComicController::class, 'store'])->name('comics.store');
 Route::put('/comics/{comic}', [ComicController::class, 'update'])->name('comics.update');
 Route::delete('/comics/{comic}', [ComicController::class, 'destroy'])->name('comics.destroy');
+Route::get('/comics/{comic}/edit', [ComicController::class, 'edit'])->name('teams.edit');
+
+
+// Route::resource('comics', ComicController::class);
